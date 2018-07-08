60d87b38fcc889f5808aac0687fb32c064717e93
Szymek <szymon.rzadca@gmail.com>
2018-07-08 14:30:56 +0200

    added centering and iterating through the list for better visual effects

--- compchallenge1.py
+++ compchallenge1.py
@@ -2,3 +2,6 @@ fizzbuzz = ["fizz buzz" if i % 15 == 0 else "fizz" if i % 5 == 0 else "buzz" if 
             else str(i) for i in range(1, 31)]
 
 print(fizzbuzz)
+
+for buzz in fizzbuzz:
+    print(buzz.center(12, "*"))
