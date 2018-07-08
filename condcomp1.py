70011950f6172541b7ef9b3301df34a67fde842f
Szymek <szymon.rzadca@gmail.com>
2018-07-08 12:19:04 +0200

    conditional expression example

---
+++ condcomp1.py
@@ -0,0 +1,26 @@
+menu = [
+    ["egg", "spam", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+    ["spam", "egg", "sausage", "spam"],
+    ["chicken", "chips"]
+]
+
+meals = []
+for meal in menu:
+    if "spam" not in meal:
+        meals.append(meal)
+    else:
+        meals.append("a meal was skipped")
+print(meals)
+
+meals = [meal for meal in menu if "spam" not in meal]
+print(meals)
+
+x = 12
+expression = "Twelve" if x==12 else "unknown"
+print(expression)
