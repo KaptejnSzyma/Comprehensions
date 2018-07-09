0c93741b9dd1a48520f785a20d595346242af6fb
Szymek <szymon.rzadca@gmail.com>
2018-07-09 17:05:58 +0200

    printing out contents of list, and not whole list at once

--- nested1.py
+++ nested1.py
@@ -1,8 +1,8 @@
 burgers = ["beef", "chicken", "spicy bean"]
 toppings = ["cheese", "egg", "beans", "spam"]
 
-meals = [(burger, topping) for burger in burgers for topping in toppings]
-print(meals)
+for meals in [(burger, topping) for burger in burgers for topping in toppings]:
+    print(meals)
 
 print()
 
